85a67e28a4dfd34bf0844042d427c8572ae7b375
rtcx2 <rtcx234@gmail.com>
2021-10-26 16:26:43 -0700

    strange-random

--- main.js
+++ main.js
@@ -6,24 +6,44 @@
 //window.screen.width;
 
 // http://www.mendoweb.be/blog/javascript-random-element-position/ Source for random position
+let play1button = document.getElementById("play1")
+let play2button = document.getElementById("play2")
+let play3button = document.getElementById("play3")
+let play4button = document.getElementById("play4")
+let play5button = document.getElementById("play5")
+let play6button = document.getElementById("play6")
+let play7button = document.getElementById("play7")
+let play8button = document.getElementById("play8")
+let play9button = document.getElementById("play9")
+
 let htmlheight = document.documentElement.clientHeight;
 let htmlwidth = document.documentElement.clientWidth;
 
-let randomheight = Math.floor(Math.random() * htmlheight);
-let randomwidth = Math.floor(Math.random() * htmlwidth);
+//let randomheight = Math.floor(Math.random() * htmlheight);
+//let randomwidth = Math.floor(Math.random() * htmlwidth);
 
-
-var myElem = document.getElementByID('play2');
-myElem.onclick = function() {
-        document.getElementById("play2").style.left = "100px";
-        document.getElementById("play2").style.left = "100px";
-        
-}
-document.getElementById("play1").onclick = function() {moveButton()};
-
-moveButton()
-
-
-
+function moveButton(){
+    let randomheight = Math.floor(Math.random() * htmlheight);
+    let randomwidth = Math.floor(Math.random() * htmlwidth);
+    play1button.style.margin = randomwidth+"px "+randomheight +"px";
+    play2button.style.margin = randomwidth+"px "+randomheight +"px";
+    play3button.style.margin = randomwidth+"px "+randomheight +"px";
+    play4button.style.margin = randomwidth+"px "+randomheight +"px";
+    play5button.style.margin = randomwidth+"px "+randomheight +"px";
+    play6button.style.margin = randomwidth+"px "+randomheight +"px";
+    play7button.style.margin = randomwidth+"px "+randomheight +"px";
+    play8button.style.margin = randomwidth+"px "+randomheight +"px";
+    play9button.style.margin = randomwidth+"px "+randomheight +"px";
+    //play1button.style.marginBottom = randomheight+"px";
+   // play1button.style.left = randomwidth+ "px";
+    //play1button.style.top = randomheight+"px";
 console.log("Document height: " + randomheight);
 console.log("Document width: " + randomwidth);
+}
+
+document.getElementById("myDiv").style.margin = "500px 100px 200px 300px";
+  
+
+
+
+
