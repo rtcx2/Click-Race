6d156097161c680f82991d5125f41f62719ec376
rtcx2 <rtcx234@gmail.com>
2021-10-26 11:36:57 -0700

    testing

---
+++ main.js
@@ -0,0 +1,8 @@
+//get the size of the document file for later
+//Source - https://stackoverflow.com/questions/3437786/get-the-size-of-the-screen-current-web-page-and-browser-window
+//$(document).height();
+//$(document).width();
+//window.screen.height;
+//window.screen.width;
+
+// http://www.mendoweb.be/blog/javascript-random-element-position/ Source for random position
