bf4e614c3306685571d4309a5190d50cca9e61dc
rtcx2 <rtcx234@gmail.com>
2021-10-26 15:13:40 -0700

    moving-buttons

--- main.js
+++ main.js
@@ -6,3 +6,24 @@
 //window.screen.width;
 
 // http://www.mendoweb.be/blog/javascript-random-element-position/ Source for random position
+let htmlheight = document.documentElement.clientHeight;
+let htmlwidth = document.documentElement.clientWidth;
+
+let randomheight = Math.floor(Math.random() * htmlheight);
+let randomwidth = Math.floor(Math.random() * htmlwidth);
+
+
+var myElem = document.getElementByID('play2');
+myElem.onclick = function() {
+        document.getElementById("play2").style.left = "100px";
+        document.getElementById("play2").style.left = "100px";
+        
+}
+document.getElementById("play1").onclick = function() {moveButton()};
+
+moveButton()
+
+
+
+console.log("Document height: " + randomheight);
+console.log("Document width: " + randomwidth);
